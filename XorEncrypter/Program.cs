8b1c124c303a582569a2586ed28e406e14871f36
Poels Thibault <thibault.poels@immalle.eu>
2018-03-06 09:18:15 +0100

    Add project files.

---
+++ XorEncrypter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XorEncrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
