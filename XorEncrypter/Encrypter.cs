3d373f6e53d9aaa676a2182b06408b12692d7ca5
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:18:26 +0200

    Program

---
+++ XorEncrypter/Encrypter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XorEncrypter
+{
+    class Encrypter
+    {
+        public static byte[] Encrypt(string cleartext, byte sleutel)
+        {
+            byte[] ciphertext = new byte[cleartext.Length];
+
+            for (int i = 0; i < cleartext.Length; i++)
+            {
+                ciphertext[i] = (byte)(cleartext[i] ^ sleutel);
+            }
+            return ciphertext;
+        }
+
+        public static string Decrypt(byte[] ciphertext, byte sleutel)
+        {
+            char[] cleartext = new char[ciphertext.Length];
+
+            for (int i = 0; i < ciphertext.Length; i++)
+            {
+                cleartext[i] = (char)(ciphertext[i] ^ sleutel);
+            }
+            return new string(cleartext);
+        }
+    }
+}
--- XorEncrypter/Program.cs
+++ XorEncrypter/Program.cs
@@ -6,7 +6,29 @@ namespace XorEncrypter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool showCiphertext = false;
+            byte sleutel = 9; // sleutel is 8 bit groot dus we kiezen byte
+            var originalText = "Hallo, dit is een groot geheim!";
+            var ciphertext = Encrypter.Encrypt(originalText, sleutel);
+            var cleartext = Encrypter.Decrypt(ciphertext, sleutel); // cleartext zou nu gelijk moeten zijn aan "hallo"
+
+            if (showCiphertext)
+            {
+                PrintBytes(ciphertext);
+            }
+            Console.WriteLine(originalText);
+            Console.WriteLine(cleartext);
+            Console.WriteLine(originalText == cleartext);
+        }
+
+        static void PrintBytes(byte[] data)
+        {
+            foreach (var b in data)
+            {
+                Console.WriteLine("0x{0,2:X} {0,5}", b);
+            }
         }
     }
+
 }
+
